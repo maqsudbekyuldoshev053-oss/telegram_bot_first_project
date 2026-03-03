6a2b904971dc994c0f76c8b79226a9d7f88c1f5f
maqsudbek <maqsudbekyuldoshev053@gmail.com>
2026-03-03 15:58:07 +0500

    first_project

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
-from pydantic import Field
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    ADMIN_ID: int = Field()
-    TELEGRAM_BOT_TOKEN: str = Field()
+    ADMIN_ID: int
+    TELEGRAM_BOT_TOKEN: str
 
     class Config:
-        env_file = ".env"
+        env_prefix = ""
 
 settings = Settings()
